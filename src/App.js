5b4d7ceaae7fb87b9fe727afba8f26e25c5dc367
unknown <oyeleyeoyedayo@gmail.com>
2021-12-08 20:48:10 +0000

    host on firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
 
 //import ChatFeed from './components/ChatFeed';
-//import LoginForm from './components/LoginForm';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const projectID = '2b01117f-6a9d-4432-90e5-a346fcb8c5f2';
 
 const App = () => {
-  //if (!localStorage.getItem('username')) return <LoginForm />;
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
     <ChatEngine
       height="100vh"
       projectID={projectID}
-      userName="Oxygeneleye"
-      userSecret="Leye0202"
-      //renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      //onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+     //S renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
